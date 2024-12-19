86dad1e7680c96fa26ce37734b18df2329caa803
Mac <vespersolutionspk@gmail.com>
2024-12-19 22:12:46 +0500

    3

--- app/ChairmanLetter.tsx
+++ app/ChairmanLetter.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const ChairmanLetter = () => {
   return (
-    <div className="p-10 mt-16 flex flex-row gap-10 h-auto min-h-screen">
-    <div className="px-10 w-1/2">
-        <h3 className="font-sans font-medium text-gray-800 text-4xl mb-5">
+    <div className="p-10 md:p-10 mt-8 md:mt-16 flex flex-col md:flex-row gap-10 h-auto min-h-screen">
+    <div className="px-0 md:px-10 w-full  md:w-1/2">
+        <h3 className="font-sans font-medium text-gray-800 text-3xl md:text-4xl mb-5">
             Chairman&apos;s <span className="text-blue-800">Message</span>
         </h3>
-        <p className="font-sans tracking-tight leading-tight font-light text-xl text-black text-justify">
+        <p className="font-sans tracking-tight leading-tight font-light text-lg md:text-xl text-black text-justify">
             I am honored to be a part of IHP, an organization driven by dedicated and determined professionals committed to improving the lives of underserved citizens and communities. Through diverse interventions in areas such as shelter, livelihoods, education, health, child development, youth development, women empowerment, WASH, peacebuilding, and more, we strive to uplift those who have been neglected and deprived of a dignified standard of living.
             <br /><br />
             Reflecting on our journey, five years ago, IHP was like a fledgling, taking its first steps. Today, it has matured and is not only walking but running with confidence and resilience. This transformation has been made possible through the unwavering trust of our stakeholders—including citizens, communities, donors, NGOs, and government agencies—and the successful execution of community-driven projects with dedication, transparency, and enthusiasm.
@@ -20,7 +20,7 @@ const ChairmanLetter = () => {
         </p>
     </div>
     <div
-        className="px-10 w-1/2 bg-cover bg-center min-h-screen rounded-3xl"
+        className="px-10 w-full md:w-1/2 bg-cover bg-center min-h-screen rounded-3xl"
         style={{ backgroundImage: `url('/assets/chairman.jpeg')` }}
     >
     </div>
