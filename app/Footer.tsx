8d4e5ef058a141f254286900e0615c24d9b402c3
Mac <vespersolutionspk@gmail.com>
2024-12-21 01:26:19 +0500

    alpha1

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+
 import React from 'react'
 import IHPLogo from "@/public/assets/iplogo.svg";
 import IHPLogopink from "@/public/assets/ihplogopink.svg";
